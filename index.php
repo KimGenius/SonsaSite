95e8da6ea13802f8b17c4a030cc4683ccab8b04a
KimGenius <geniusk1047@naver.com>
2017-12-13 00:50:58 +0900

    footer 부분 주소+연락처 추가

--- index.php
+++ index.php
@@ -162,31 +162,6 @@
     </div>
 </section>
 
-<!-- About Section -->
-<section class="bg-primary text-white mb-0" id="about">
-    <div class="container">
-        <h2 class="text-center text-uppercase text-white">About</h2>
-        <hr class="star-light mb-5">
-        <div class="row">
-            <div class="col-lg-4 ml-auto">
-                <p class="lead">Freelancer is a free bootstrap theme created by Start Bootstrap. The download includes
-                    the complete source files including HTML, CSS, and JavaScript as well as optional LESS stylesheets
-                    for easy customization.</p>
-            </div>
-            <div class="col-lg-4 mr-auto">
-                <p class="lead">Whether you're a student looking to showcase your work, a professional looking to
-                    attract clients, or a graphic artist looking to share your projects, this template is the perfect
-                    starting point!</p>
-            </div>
-        </div>
-        <div class="text-center mt-4">
-            <a class="btn btn-xl btn-outline-light" href="#">
-                <i class="fa fa-download mr-2"></i>
-                Download Now!
-            </a>
-        </div>
-    </div>
-</section>
 
 <!-- Contact Section -->
 <section id="contact">
@@ -248,45 +223,45 @@
 <footer class="footer text-center">
     <div class="container">
         <div class="row">
-            <div class="col-md-4 mb-5 mb-lg-0">
+            <div class="col-md-6 mb-5 mb-lg-0">
                 <h4 class="text-uppercase mb-4">Location</h4>
-                <p class="lead mb-0">2215 John Daniel Drive
-                    <br>Clark, MO 65243</p>
+                <p class="lead mb-0">경기도 부천시 장말로 376<br/>203호(심곡동,성보빌딩)</p>
             </div>
-            <div class="col-md-4 mb-5 mb-lg-0">
-                <h4 class="text-uppercase mb-4">Around the Web</h4>
-                <ul class="list-inline mb-0">
-                    <li class="list-inline-item">
-                        <a class="btn btn-outline-light btn-social text-center rounded-circle" href="#">
-                            <i class="fa fa-fw fa-facebook"></i>
-                        </a>
-                    </li>
-                    <li class="list-inline-item">
-                        <a class="btn btn-outline-light btn-social text-center rounded-circle" href="#">
-                            <i class="fa fa-fw fa-google-plus"></i>
-                        </a>
-                    </li>
-                    <li class="list-inline-item">
-                        <a class="btn btn-outline-light btn-social text-center rounded-circle" href="#">
-                            <i class="fa fa-fw fa-twitter"></i>
-                        </a>
-                    </li>
-                    <li class="list-inline-item">
-                        <a class="btn btn-outline-light btn-social text-center rounded-circle" href="#">
-                            <i class="fa fa-fw fa-linkedin"></i>
-                        </a>
-                    </li>
-                    <li class="list-inline-item">
-                        <a class="btn btn-outline-light btn-social text-center rounded-circle" href="#">
-                            <i class="fa fa-fw fa-dribbble"></i>
-                        </a>
-                    </li>
-                </ul>
-            </div>
-            <div class="col-md-4">
-                <h4 class="text-uppercase mb-4">About Freelancer</h4>
-                <p class="lead mb-0">Freelance is a free to use, open source Bootstrap theme created by
-                    <a href="http://startbootstrap.com">Start Bootstrap</a>.</p>
+<!--            <div class="col-md-4 mb-5 mb-lg-0">-->
+<!--                <h4 class="text-uppercase mb-4">Around the Web</h4>-->
+<!--                <ul class="list-inline mb-0">-->
+<!--                    <li class="list-inline-item">-->
+<!--                        <a class="btn btn-outline-light btn-social text-center rounded-circle" href="#">-->
+<!--                            <i class="fa fa-fw fa-facebook"></i>-->
+<!--                        </a>-->
+<!--                    </li>-->
+<!--                    <li class="list-inline-item">-->
+<!--                        <a class="btn btn-outline-light btn-social text-center rounded-circle" href="#">-->
+<!--                            <i class="fa fa-fw fa-google-plus"></i>-->
+<!--                        </a>-->
+<!--                    </li>-->
+<!--                    <li class="list-inline-item">-->
+<!--                        <a class="btn btn-outline-light btn-social text-center rounded-circle" href="#">-->
+<!--                            <i class="fa fa-fw fa-twitter"></i>-->
+<!--                        </a>-->
+<!--                    </li>-->
+<!--                    <li class="list-inline-item">-->
+<!--                        <a class="btn btn-outline-light btn-social text-center rounded-circle" href="#">-->
+<!--                            <i class="fa fa-fw fa-linkedin"></i>-->
+<!--                        </a>-->
+<!--                    </li>-->
+<!--                    <li class="list-inline-item">-->
+<!--                        <a class="btn btn-outline-light btn-social text-center rounded-circle" href="#">-->
+<!--                            <i class="fa fa-fw fa-dribbble"></i>-->
+<!--                        </a>-->
+<!--                    </li>-->
+<!--                </ul>-->
+<!--            </div>-->
+            <div class="col-md-6">
+                <h4 class="text-uppercase mb-4">Tell Us</h4>
+                <p class="lead mb-0">
+                    032 655 0002<br/>
+                    010 8870 5510</p>
             </div>
         </div>
     </div>
@@ -294,7 +269,7 @@
 
 <div class="copyright py-4 text-center text-white">
     <div class="container">
-        <small>Copyright &copy; Your Website 2017</small>
+        <small>Copyright &copy; GeniusK 2017</small>
     </div>
 </div>
 
@@ -303,152 +278,6 @@
     <a class="js-scroll-trigger d-block text-center text-white rounded" href="#page-top">
         <i class="fa fa-chevron-up"></i>
     </a>
-</div>
-
-<!-- Portfolio Modals -->
-
-<!-- Portfolio Modal 1 -->
-<div class="portfolio-modal mfp-hide" id="portfolio-modal-1">
-    <div class="portfolio-modal-dialog bg-white">
-        <a class="close-button d-none d-md-block portfolio-modal-dismiss" href="#">
-            <i class="fa fa-3x fa-times"></i>
-        </a>
-        <div class="container text-center">
-            <div class="row">
-                <div class="col-lg-8 mx-auto">
-                    <h2 class="text-secondary text-uppercase mb-0">Project Name</h2>
-                    <hr class="star-dark mb-5">
-                    <img class="img-fluid mb-5" src="images/portfolio/cabin.png" alt="">
-                    <p class="mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia neque assumenda
-                        ipsam nihil, molestias magnam, recusandae quos quis inventore quisquam velit asperiores, vitae?
-                        Reprehenderit soluta, eos quod consequuntur itaque. Nam.</p>
-                    <a class="btn btn-primary btn-lg rounded-pill portfolio-modal-dismiss" href="#">
-                        <i class="fa fa-close"></i>
-                        Close Project</a>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-
-<!-- Portfolio Modal 2 -->
-<div class="portfolio-modal mfp-hide" id="portfolio-modal-2">
-    <div class="portfolio-modal-dialog bg-white">
-        <a class="close-button d-none d-md-block portfolio-modal-dismiss" href="#">
-            <i class="fa fa-3x fa-times"></i>
-        </a>
-        <div class="container text-center">
-            <div class="row">
-                <div class="col-lg-8 mx-auto">
-                    <h2 class="text-secondary text-uppercase mb-0">Project Name</h2>
-                    <hr class="star-dark mb-5">
-                    <img class="img-fluid mb-5" src="images/portfolio/cake.png" alt="">
-                    <p class="mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia neque assumenda
-                        ipsam nihil, molestias magnam, recusandae quos quis inventore quisquam velit asperiores, vitae?
-                        Reprehenderit soluta, eos quod consequuntur itaque. Nam.</p>
-                    <a class="btn btn-primary btn-lg rounded-pill portfolio-modal-dismiss" href="#">
-                        <i class="fa fa-close"></i>
-                        Close Project</a>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-
-<!-- Portfolio Modal 3 -->
-<div class="portfolio-modal mfp-hide" id="portfolio-modal-3">
-    <div class="portfolio-modal-dialog bg-white">
-        <a class="close-button d-none d-md-block portfolio-modal-dismiss" href="#">
-            <i class="fa fa-3x fa-times"></i>
-        </a>
-        <div class="container text-center">
-            <div class="row">
-                <div class="col-lg-8 mx-auto">
-                    <h2 class="text-secondary text-uppercase mb-0">Project Name</h2>
-                    <hr class="star-dark mb-5">
-                    <img class="img-fluid mb-5" src="images/portfolio/circus.png" alt="">
-                    <p class="mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia neque assumenda
-                        ipsam nihil, molestias magnam, recusandae quos quis inventore quisquam velit asperiores, vitae?
-                        Reprehenderit soluta, eos quod consequuntur itaque. Nam.</p>
-                    <a class="btn btn-primary btn-lg rounded-pill portfolio-modal-dismiss" href="#">
-                        <i class="fa fa-close"></i>
-                        Close Project</a>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-
-<!-- Portfolio Modal 4 -->
-<div class="portfolio-modal mfp-hide" id="portfolio-modal-4">
-    <div class="portfolio-modal-dialog bg-white">
-        <a class="close-button d-none d-md-block portfolio-modal-dismiss" href="#">
-            <i class="fa fa-3x fa-times"></i>
-        </a>
-        <div class="container text-center">
-            <div class="row">
-                <div class="col-lg-8 mx-auto">
-                    <h2 class="text-secondary text-uppercase mb-0">Project Name</h2>
-                    <hr class="star-dark mb-5">
-                    <img class="img-fluid mb-5" src="images/portfolio/game.png" alt="">
-                    <p class="mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia neque assumenda
-                        ipsam nihil, molestias magnam, recusandae quos quis inventore quisquam velit asperiores, vitae?
-                        Reprehenderit soluta, eos quod consequuntur itaque. Nam.</p>
-                    <a class="btn btn-primary btn-lg rounded-pill portfolio-modal-dismiss" href="#">
-                        <i class="fa fa-close"></i>
-                        Close Project</a>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-
-<!-- Portfolio Modal 5 -->
-<div class="portfolio-modal mfp-hide" id="portfolio-modal-5">
-    <div class="portfolio-modal-dialog bg-white">
-        <a class="close-button d-none d-md-block portfolio-modal-dismiss" href="#">
-            <i class="fa fa-3x fa-times"></i>
-        </a>
-        <div class="container text-center">
-            <div class="row">
-                <div class="col-lg-8 mx-auto">
-                    <h2 class="text-secondary text-uppercase mb-0">Project Name</h2>
-                    <hr class="star-dark mb-5">
-                    <img class="img-fluid mb-5" src="images/portfolio/safe.png" alt="">
-                    <p class="mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia neque assumenda
-                        ipsam nihil, molestias magnam, recusandae quos quis inventore quisquam velit asperiores, vitae?
-                        Reprehenderit soluta, eos quod consequuntur itaque. Nam.</p>
-                    <a class="btn btn-primary btn-lg rounded-pill portfolio-modal-dismiss" href="#">
-                        <i class="fa fa-close"></i>
-                        Close Project</a>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-
-<!-- Portfolio Modal 6 -->
-<div class="portfolio-modal mfp-hide" id="portfolio-modal-6">
-    <div class="portfolio-modal-dialog bg-white">
-        <a class="close-button d-none d-md-block portfolio-modal-dismiss" href="#">
-            <i class="fa fa-3x fa-times"></i>
-        </a>
-        <div class="container text-center">
-            <div class="row">
-                <div class="col-lg-8 mx-auto">
-                    <h2 class="text-secondary text-uppercase mb-0">Project Name</h2>
-                    <hr class="star-dark mb-5">
-                    <img class="img-fluid mb-5" src="images/portfolio/submarine.png" alt="">
-                    <p class="mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia neque assumenda
-                        ipsam nihil, molestias magnam, recusandae quos quis inventore quisquam velit asperiores, vitae?
-                        Reprehenderit soluta, eos quod consequuntur itaque. Nam.</p>
-                    <a class="btn btn-primary btn-lg rounded-pill portfolio-modal-dismiss" href="#">
-                        <i class="fa fa-close"></i>
-                        Close Project</a>
-                </div>
-            </div>
-        </div>
-    </div>
 </div>
 
 <!-- Bootstrap core JavaScript -->
