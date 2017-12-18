79d5fcac29ab4d170bab371c0a33370df8c25696
KimGenius <geniusk1047@naver.com>
2017-12-19 01:53:08 +0900

    file download 및 팝업창 내용 보충

--- index.php
+++ index.php
@@ -31,37 +31,6 @@
 
 <body id="page-top">
 
-<div class="about-popup">
-    <div class="about-popup-top">
-        <span>X</span>
-        <img src="/images/about/1.png" alt="1.png">
-        <div class="about-popup-top-div">
-            <img src="/images/about/icon1.png" alt="icon1">
-            <p><span>인보험 손해사정</span><br/>
-                질병 및 상해보험 손해사정 후유장해 손해사정</p>
-        </div>
-    </div>
-    <div class="about-popup-bottom">
-        <div class="about-popup-bottom-div">
-            <p>1. AMA 평가방법이란?</p>
-            <p>AMA식 평가방법이란 미국의학협회(American Medical Association)의 가과 전문의가<br/>
-                각기 전문분야의 영구적 신체장해에 대한 장해율을 정한 지침입니다.<br/>
-                개인보험에서는 AMA 방식을 적용하여 피보험자의 직업, 연령, 소득 등과 관계없이 주로 일상생활에의 제한정도를 종합하여<br/>
-                평가하는 일종의 자연과학적 방법으로서 장해를 백분율(%)로 구분하여 평가하는 장해지급율표 방식을 2005.04월부터<br/>
-                생명보험과 손해보험 공통 적용합니다.</p>
-        </div>
-        <div class="about-popup-bottom-div">
-            <p>2. AMA평가 방법의 특징은?</p>
-            <p>가. 영구적 신체장해만 취급하며, 한시적 장해는 평가에서 제외됩니다.<br/>
-                나. 일부 주요관절의 운동의 두개 내지 세개의 운동종류별로 나누어서 그 기능적 비례치를 표시하고 있어 장해평가가 정밀합니다.<br/>
-                다. AMA식 운동범위평가에서 모든 관절의 운동범위는 중립위치를 0°로 해서 기록하며 관절운동은 0°를 시점으로 합니다.</p>
-        </div>
-        <div class="about-popup-bottom-div">
-            <p>3. AMA 평가 기준 서식 다운로드</p>
-            <span>AMA 신체 제관절 운동각도</span><span>생명 손해보험 통합 후유장해분류표</span>
-        </div>
-    </div>
-</div>
 <!-- Navigation -->
 <nav class="navbar navbar-expand-lg text-uppercase" id="mainNav">
     <div class="container">
@@ -111,7 +80,7 @@
         <p class="custom-about-text">청률손해사정은 고객의 편에 서서 최고의 서비스를 제공합니다.</p>
         <div class="row">
             <div class="col-md-8">
-                <div class="about-us-item">
+                <div class="about-us-item about-us-item1">
                     <div class="about-us-item-top">
                         <img src="images/about/1.png" alt="1.png">
                         <img src="images/about/icon1.png" alt="icon1.png">
@@ -127,7 +96,7 @@
                 </div>
             </div>
             <div class="col-md-8">
-                <div class="about-us-item">
+                <div class="about-us-item about-us-item2">
                     <div class="about-us-item-top">
                         <img src="images/about/2.png" alt="1.png">
                         <img src="images/about/icon2.png" alt="icon1.png">
@@ -143,7 +112,7 @@
                 </div>
             </div>
             <div class="col-md-8">
-                <div class="about-us-item">
+                <div class="about-us-item about-us-item3">
                     <div class="about-us-item-top">
                         <img src="images/about/3.png" alt="2.png">
                         <img src="images/about/icon3.png" alt="icon2.png">
@@ -159,7 +128,7 @@
                 </div>
             </div>
             <div class="col-md-8">
-                <div class="about-us-item">
+                <div class="about-us-item about-us-item4">
                     <div class="about-us-item-top">
                         <img src="images/about/4.png" alt="3.png">
                         <img src="images/about/icon4.png" alt="icon3.png">
@@ -175,7 +144,7 @@
                 </div>
             </div>
             <div class="col-md-8">
-                <div class="about-us-item">
+                <div class="about-us-item about-us-item5">
                     <div class="about-us-item-top">
                         <img src="images/about/5.png" alt="4.png">
                         <img src="images/about/icon5.png" alt="icon4.png">
@@ -292,6 +261,176 @@
     <a class="js-scroll-trigger d-block text-center text-white rounded" href="#page-top">
         <i class="fa fa-chevron-up"></i>
     </a>
+</div>
+
+<!-- popup -->
+<div class="about-popup about-popup1">
+    <div class="about-popup-top">
+        <span class="about-popup-disable">X</span>
+        <img src="/images/about/1.png" alt="1.png">
+        <div class="about-popup-top-div">
+            <img src="/images/about/icon1.png" alt="icon1">
+            <p><span>인보험 손해사정</span><br/>
+                질병 및 상해보험 손해사정 후유장해 손해사정</p>
+        </div>
+    </div>
+    <div class="about-popup-bottom">
+        <div class="about-popup-bottom-div">
+            <p>1. AMA 평가방법이란?</p>
+            <p>AMA식 평가방법이란 미국의학협회(American Medical Association)의 가과 전문의가<br/>
+                각기 전문분야의 영구적 신체장해에 대한 장해율을 정한 지침입니다.<br/>
+                개인보험에서는 AMA 방식을 적용하여 피보험자의 직업, 연령, 소득 등과 관계없이 주로 일상생활에의 제한정도를 종합하여<br/>
+                평가하는 일종의 자연과학적 방법으로서 장해를 백분율(%)로 구분하여 평가하는 장해지급율표 방식을 2005.04월부터<br/>
+                생명보험과 손해보험 공통 적용합니다.</p>
+        </div>
+        <div class="about-popup-bottom-div">
+            <p>2. AMA평가 방법의 특징은?</p>
+            <p>가. 영구적 신체장해만 취급하며, 한시적 장해는 평가에서 제외됩니다.<br/>
+                나. 일부 주요관절의 운동의 두개 내지 세개의 운동종류별로 나누어서 그 기능적 비례치를 표시하고 있어 장해평가가 정밀합니다.<br/>
+                다. AMA식 운동범위평가에서 모든 관절의 운동범위는 중립위치를 0°로 해서 기록하며 관절운동은 0°를 시점으로 합니다.</p>
+        </div>
+        <!--        <div class="about-popup-bottom-div">-->
+        <!--            <p>3. AMA 평가 기준 서식 다운로드</p>-->
+        <!--            <span>AMA 신체 제관절 운동각도</span><span>생명 손해보험 통합 후유장해분류표</span>-->
+        <!--        </div>-->
+    </div>
+</div>
+
+<div class="about-popup about-popup2">
+    <div class="about-popup-top">
+        <span class="about-popup-disable">X</span>
+        <img src="/images/about/2.png" alt="2.png">
+        <div class="about-popup-top-div">
+            <img src="/images/about/icon2.png" alt="icon2">
+            <p><span>교통사고 손해사정</span><br/>
+                교통사고로 인한 상해 및 후유장해 손해사정
+            </p>
+        </div>
+    </div>
+    <div class="about-popup-bottom">
+        <div class="about-popup-bottom-div">
+            <p>1. 맥브라이드(Mc Bride) 평가방법이란?</p>
+            <p>미국 오클라호마대학교 의과대학의 Earl D.Mc Bride 교수가 저술한 Compensable injuries에 대한 ‘노동능력상실평가와 치료의 원칙’이라는 책에<br/>
+                장해의 부위, 종류, 정도에 따라 정밀하게 장해율을 평가할 수 있도록 되어 있습니다. <br/>
+                맥브라이드는 A.M.A와 마찬가지로 순수한 신체장해율을 먼저 구하고 여기에 직업계수에 따른 장해율표를<br/>
+                적용하여 최종적으로 노동능력상실율을 산출하도록 구성되어 있습니다.</p>
+        </div>
+        <div class="about-popup-bottom-div">
+            <p>2. 맥브라이드 평가 기준</p>
+            <p>-> 맥브라이드식 노동능력상실 평가기준<br/>
+                -> 맥브라이드식 노동능력상실률표<br/>
+                -> 직업별 손상부위에 대한 장해계수</p>
+        </div>
+        <div class="about-popup-bottom-div">
+            <p>3. 맥브라이드 평가 기준 서식 다운로드</p>
+            <span class="mac-bride1">맥 브라이드식 노동능력상실 평가기준</span>
+            <span class="mac-bride2">맥 브라이드식 노동능력상실률표</span>
+        </div>
+    </div>
+</div>
+
+
+<div class="about-popup about-popup3">
+    <div class="about-popup-top">
+        <span class="about-popup-disable">X</span>
+        <img src="/images/about/3.png" alt="3.png">
+        <div class="about-popup-top-div">
+            <img src="/images/about/icon3.png" alt="icon3">
+            <p><span>지급거절 보험금</span><br/>
+                지급 거절된 보험금 재손해사정</p>
+        </div>
+    </div>
+    <div class="about-popup-bottom">
+        <div class="about-popup-bottom-div">
+            <p>1. 보험사로부터 보험금 지급 거절이 된 경우 어떻게 해야하나요?</p>
+            <p>지급 거절의 사유에는 여러 이유가 있을 수 있습니다. <br/>
+                먼저 보험계약자의 3대 의무 위반을 사유로 거절할 수 있는데요. 3대 의무는 다음과 같습니다.
+                1. 보험료 납입의무 위반으로 인한 실효<br/>
+                2. 계약 전 알릴의무(고지의무) 위반으로 인한 거절<br/>
+                3. 계약 후 알릴의무(통지의무) 위반으로 인한 거절</p>
+        </div>
+        <div class="about-popup-bottom-div">
+            <p>위의 2번과 3번의 경우 많은 분들이 자신이 알지 못해서 또는 알고 있어도<br/>
+                고지하는 것을 잊거나 고지하는 방법을 알지 못해 위반하는 경우가 많습니다.<br/>
+                이럴 경우 어떻게 보상 받아야할까요?</p>
+            <p>고지의무와 통지의무 위반의 제척기간이 경과되었는지를 확인하는 것이 가장 대표적인데요.<br/>
+                이 외 보험금을 지급받을 수 있는지 확인하고 싶으시다면 언제든 홈페이지 내 상담예약 또는 전화를 통해 문의해 주시기 바랍니다!
+            </p>
+        </div>
+    </div>
+</div>
+
+
+<div class="about-popup about-popup4">
+    <div class="about-popup-top">
+        <span class="about-popup-disable">X</span>
+        <img src="/images/about/4.png" alt="4.png">
+        <div class="about-popup-top-div">
+            <img src="/images/about/icon4.png" alt="icon4">
+            <p><span>산재 후 근재 손해사정</span><br/>
+                산재 휴유등급 판정 안내 및 근재 보험금 손해사정</p>
+        </div>
+    </div>
+    <div class="about-popup-bottom">
+        <div class="about-popup-bottom-div">
+            <p>1. 산업재해란?</p>
+            <p>산업재해(산재)란 근로자가 임금을 목적으로 사업주에게 고용되어 일하던 도중에 부상, 사망하거나(사고성 재해),<br/>
+                일정한 일을 오랫동안 하면서 그 일에 따르는 유해한 작업환경이나 작업자세로 인해 서서히 발생하는 질병(직업병)으로서,<br/>
+                4일 이상의 요양을 요하는 경우를 말합니다. 사고성 재해는 ‘업무상 재해’, 직업병은 ‘업무상 질병’ 이라는 용어를 사용힙니다.</p>
+        </div>
+        <div class="about-popup-bottom-div">
+            <p>2. 근재보상이란?</p>
+            <p>근재보상이란 일정한 사업장에 고용된 근로자가 업무수행중 불의의 재해를 입을 경우에 사용자가 부담하여야 할 근로기준법,<br/>
+                산업재해보상보험법과 민법상 사용자가 추가로 부담하게 되는 법률상의 배상책임 손해를 보상하여 주는 것을 말합니다.<br/>
+                따라서 근로자는 근로복지공단에서 보상하는 산재보험급여를 초과하는 민사상의 손해배상에 대하여는 건설업체 등에서<br/>
+                가입하고 있는 손해보험회사에 근재 보험금을 청구할 수 있게 됩니다.
+            </p>
+        </div>
+        <div class="about-popup-bottom-div">
+            <p>3. 산재보험 신체 장해 기준 서식 다운로드</p>
+            <span class="sanjae">산재 신체장해 등급표</span>
+        </div>
+    </div>
+</div>
+
+
+<div class="about-popup about-popup5">
+    <div class="about-popup-top">
+        <span class="about-popup-disable">X</span>
+        <img src="/images/about/5.png" alt="5.png">
+        <div class="about-popup-top-div">
+            <img src="/images/about/icon5.png" alt="icon5">
+            <p><span>합의 종결 사건 재손해사정</span><br/>
+                분쟁 조정, 사고 재조사</p>
+        </div>
+    </div>
+    <div class="about-popup-bottom">
+        <div class="about-popup-bottom-div">
+            <p>1. 합의가 무효 또는 취소가 되는 경우는?</p>
+            <p>
+                (1) 합의과정에서 의료분쟁의 한쪽 당사자가 경제적으로 매우 어려운 사정에 있거나 혹은 경솔하여 지나치게 불공평한 합의를 하였다면 불공정한<br/>
+                법률행위로 무효가 될 수 있습니다(「민법」 제104조).<br/>
+                (2)이미 합의가 된 내용은 합의 이후에 취소할 수 없습니다(「민법」 제733조).<br/>
+                다만, 합의 내용에 포함되지 않았던 내용(예를 들어 수술 후 관리비용 등)에 대해서는 추가로 합의할 수 있습니다.<br/>
+                또한, 의료분쟁의 한쪽 당사자가 과실이 있던 내용을 감추고 합의에 임했던 사실이 인정되면 사기로 인한 취소가 가능합니다(「민법」 제110조).
+            </p>
+        </div>
+        <div class="about-popup-bottom-div">
+            <p>2. 이미 합의가 끝난 사건을 재손해사정할 수 있나요?</p>
+            <p>네 가능합니다.교통사고에 대한 부상에 관하여 합의를 하면 추가청구를 할 수 없는 것이 원칙입니다.<br/>
+                그러나 합의 당시 예측할 수 없었던 손해(후유증 등)라는 특별한 사정이 있고, 그러한 사정을 증명할 수 있다면 위 합의에도 불구하고<br/>
+                추가청구를 할 수 있습니다(「민법」 제732조 및 733조 참조).<br/>
+                또한 판례도 “불법행위로 인한 손해배상에 관하여 가해자와 피해자 사이에 피해자가 일정한 금액을 지급받고 그 나머지 청구를 포기하기로<br/>
+                합의가 이루어진 때에는 그 후 그 이상의 손해가 발생하였다 하여 다시 그 배상을 청구할 수 없는 것이지만,<br/>
+                그 합의가 손해의 범위를 정확히 확인하기 어려운 상황에서 이루어진 것이고, 후발손해가 합의 당시의 사정으로 보아 예상이 불가능한 것으로서,<br/>
+                당사자가 후발손해를 예상하였더라면 사회통념상 그 합의금액으로는 화해하지 않았을 것이라고 보는 것이 상당할 만큼<br/>
+                그 손해가 중대한 것일 때에는 당사자의 의사가 이러한 손해에 대해서까지 그 배상청구권을 포기한 것이라고 볼 수 없으므로<br/>
+                다시 그 배상을 청구할 수 있다”고 판시하고 있습니다(대법원 2001.9.14 선고, 99다42797, 판결).<br/><br/>
+                따라서 교통사고 합의를 했다고 하더라도 예측할 수 없는 손해가 발생한 경우라면 추가적인 배상청구를 할 수 있습니다. <br/>
+                다만, 위의 특별한 사정은 추가적인 배상청구를 하려는 자가 증명을 해야 합니다.
+            </p>
+        </div>
+    </div>
 </div>
 
 <!-- Bootstrap core JavaScript -->
--- javascripts/custom.js
+++ javascripts/custom.js
@@ -1,8 +1,47 @@
-$(function() {
-    $('.about-popup-top > span').click(function() {
-        $('.about-popup').fadeOut()
+$(function () {
+    $('.about-popup1 .about-popup-disable').click(function () {
+        $('.about-popup1').fadeOut()
     })
-    $('.about-us-item').click(function() {
-        $('.about-popup').fadeIn()
+    $('.about-us-item1').click(function () {
+        $('.about-popup1').fadeIn()
+    })
+
+    $('.about-popup2 .about-popup-disable').click(function () {
+        $('.about-popup2').fadeOut()
+    })
+    $('.about-us-item2').click(function () {
+        $('.about-popup2').fadeIn()
+    })
+
+    $('.about-popup3 .about-popup-disable').click(function () {
+        $('.about-popup3').fadeOut()
+    })
+    $('.about-us-item3').click(function () {
+        $('.about-popup3').fadeIn()
+    })
+
+    $('.about-popup4 .about-popup-disable').click(function () {
+        $('.about-popup4').fadeOut()
+    })
+    $('.about-us-item4').click(function () {
+        $('.about-popup4').fadeIn()
+    })
+
+    $('.about-popup5 .about-popup-disable').click(function () {
+        $('.about-popup5').fadeOut()
+    })
+    $('.about-us-item5').click(function () {
+        $('.about-popup5').fadeIn()
+    })
+
+    // file download
+    $('.mac-bride1').click(function () {
+        window.location.assign('/file/mac-bride1.pdf');
+    })
+    $('.mac-bride2').click(function () {
+        window.location.assign('/file/mac-bride2.pdf');
+    })
+    $('.sanjae').click(function () {
+        window.location.assign('/file/sanjae.pdf');
     })
 })
